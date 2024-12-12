a9ec933a4045b4723493a9d62f49501dcd1fa976
Fatmir Margegaj <fatmir.margegaj@getabstract.com>
2024-12-12 14:22:07 +0100

    linter test

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 import chalk from "chalk";
 
 console.log(chalk.blue('It works'));
+
+let myUnusedVariable = 10; // Löst 'no-unused-vars' und 'prefer-const' aus
+if (myUnusedVariable == 10) { // Löst 'eqeqeq' aus
+    console.log('This is an example of bad code.');
+}
